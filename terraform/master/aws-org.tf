506ffc1283888eb9cca17f0e3b54856600d7feab
Keith Rhea <keithr@MPGLT-00359.local>
2019-11-23 14:12:42 -0700

    Imported AWS Organization

--- terraform/master/aws-org.tf
+++ terraform/master/aws-org.tf
@@ -2,7 +2,11 @@ resource "aws_organizations_organization" "org" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
     "config.amazonaws.com",
+    "sso.amazonaws.com"
   ]
 
   feature_set = "ALL"
+  enabled_policy_types = [
+    "SERVICE_CONTROL_POLICY",
+    ]
 }
